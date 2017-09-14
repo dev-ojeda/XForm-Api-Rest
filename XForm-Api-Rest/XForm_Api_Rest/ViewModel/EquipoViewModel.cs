74bec629a82f543f2f3bce4e96196f2bce2b8033
Devojeda <hxc1981@gmail.com>
2017-09-14 20:20:52 -0300

    Xamarin

---
+++ XForm-Api-Rest/XForm_Api_Rest/ViewModel/EquipoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XForm_Api_Rest.ViewModel
+{
+    using System.Collections.Generic;
+    using XForm_Api_Rest.Model;
+
+    public class EquipoViewModel
+    {
+        public List<EquipoModel> ListarEquipo { get; set; }
+
+        public EquipoViewModel() => ListarEquipo = new EquipoModel().ListarEquipo();
+    }
+}
